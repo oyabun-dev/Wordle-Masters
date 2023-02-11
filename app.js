bdf5b6acee693ae03facd348f0da541b19d3a8d0
oyabun-dev <khalifaababacarbeye@esp.sn>
2023-02-11 21:02:29 +0000

    Dark mode done 😎🌚🌞

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 console.log("Konichiwa, nakama");
 console.log("Omae wa mou shindeiru");
+const main = document.querySelector('#main');
+const input = document.querySelector('.input');
+const columns = document.querySelectorAll('.columns');
+const themeSwitcher = document.querySelector('#switch');
+
+themeSwitcher.addEventListener('click', darkMode);
+function darkMode () {
+    main.classList.toggle('dark');
+    input.classList.toggle('dark');
+    if (input.classList.contains('dark')) {
+        columns.forEach(column => {
+            column.style.border = '1px solid var(--dark-theme-border)';
+        })
+    } else {
+        columns.forEach(column => {
+            column.style.border = '1px solid var(--light-theme-border)';
+        })
+    }
+}
